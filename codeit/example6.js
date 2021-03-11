3b8b7e6434bed34a6837bda343ee95dcc230271e
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-11 18:27:43 +0900

    03_11_study

---
+++ codeit/example6.js
@@ -0,0 +1,10 @@
+const introduction = (name, birth, nationality, job) => {
+    console.log('안녕하세요! 방갑습니다.');
+    console.log('제 이름은 ' + name + ' 입니다.');
+    console.log('생년월일은 ' + birth + ' 입니다.');
+    console.log('국적은 ' + nationality + ' 입니다.');
+    console.log('직업은 ' + job + ' 입니다.');
+    console.log('잘 부탁드리겠습니다.!');
+};
+
+introduction('코드잇', '17.05.18', '한국', '프로그래밍 강사');
---
+++ codeit/example7.js
@@ -0,0 +1,8 @@
+const getTwice = number => {
+    return number * 2;
+};
+
+const x = getTwice(5);
+const y = getTwice(2);
+
+console.log(x * y);
