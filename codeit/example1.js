c39ccb8a0e6b30c332e71c8cb23ce82d90ac36dd
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-11 17:51:58 +0900

    03_11_study

---
+++ codeit/example1.js
@@ -0,0 +1,11 @@
+// console.log(10 + 5);
+
+console.log(20 - 5);
+
+/*
+아래 코드는
+
+브라우저 콘솔이 15를 출력하는 코드입니다.
+*/
+console.log(10 + 5);
+console.log(20 - 5);
---
+++ codeit/example3.js
@@ -0,0 +1,22 @@
+function greeting() {
+    console.log('Hi');
+    console.log('안녕');
+    console.log('(일본어)안녕')
+    console.log('(중국어)안녕')
+    console.log('(몰라..)안녕')
+    console.log('(프랑스)안녕')
+    console.log('(어딘지 몰라 ㅠㅠ)안녕')
+};
+
+// 함수 호출
+greeting();
+greeting();
+
+
+console.log('Hi');
+console.log('안녕');
+console.log('(일본어)안녕')
+console.log('(중국어)안녕')
+console.log('(몰라..)안녕')
+console.log('(프랑스)안녕')
+console.log('(어딘지 몰라 ㅠㅠ)안녕')
