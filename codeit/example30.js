9075a391e92a14ace044860e233bffe5f2bb6f9e
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-16 22:31:09 +0900

    03_16_study

--- codeit/example30.js
+++ codeit/example30.js
@@ -1,13 +1,13 @@
-// 파라미터 height을 활용하는 checkHeight 함수를 완성하세요.
-function checkHeight(height) {
-	if (height >= 140) {
-        console.log("탑승이 가능합니다.");
+// if문 (if statement)
+
+const temperature = 80;
+
+if (temperature <= 0) {
+    console.log('물이 업니다.');
+} else {
+    if (temperature < 100) {
+        console.log('물이 얼지도 끓지도 않습니다.');
     } else {
-        console.log('탑승이 불가능합니다.');
+        console.log('물이 끓습니다.');
     }
 }
-
-// 테스트 코드
-checkHeight(141);
-checkHeight(135);
-checkHeight(170);
---
+++ codeit/example33.js
@@ -0,0 +1,5 @@
+// switch문 (switch statement)
+
+/**
+ * 당신이 등산을 하던 중 ...
+ */
