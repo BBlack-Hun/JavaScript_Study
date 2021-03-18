7b01fcf4a5380abf2fe8d25636ef125b4a057614
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-18 23:00:20 +0900

    03_18_study

---
+++ codeit/Program_and_Data/example18.js
@@ -0,0 +1,21 @@
+// 배열의 메소드 (Array's Method)
+
+let members = ['쿤갈래', 'Zerrard66', '우리생각해써', '흙토끼', 'End Miracle'];
+
+console.log(members);
+
+// 배열의 첫요소를 삭제: Shift();
+members.shift();
+console.log(members);
+
+// 배열의 마지막 요소를 삭제: pop();
+members.pop();
+console.log(members);
+
+// 배열의 첫 요소로 값 추가: unshift(value);
+members.unshift('NiceCodeit');
+console.log(members);
+
+// 배열의 마지막 요소로 값 추가: push(value);
+members.push('HiCodeit');
+console.log(members);
---
+++ codeit/Program_and_Data/example20.js
@@ -0,0 +1,15 @@
+// for.... of
+
+let influencer = ['suwonlog', 'small.tiger', 'Minam.ludens', 'su_convenience24'];
+
+for (let i = 0; i < influencer.length; i++) {
+    console.log(influencer[i]);
+}
+
+for (let element of influencer) {
+    console.log(element);
+}
+
+for (let index in influencer) {
+    console.log(influencer[index]);
+}
