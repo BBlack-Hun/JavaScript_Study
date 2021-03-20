3be5aa12de4c5409d0d8c9e68c52b61c6ea528ed
bblack_hun <bblack_hun@bblack-hun.local>
2021-03-20 16:10:04 +0900

    03_20_study

--- codeit/Program_and_Data/example25.js
+++ codeit/Program_and_Data/example25.js
@@ -1,24 +1,5 @@
-// Number
+// 문자열
+let myString = 'Hi Codeit!';
 
-let myNumber = 0.3591;
-
-// toFixed(0 ~ 100)
-console.log(myNumber.toFixed(7));
-
-console.log(typeof myNumber.toFixed(7));
-
-console.log(Number(myNumber.toFixed(7)));
-
-console.log(+myNumber.toFixed(7));
-
-console.log(+myNumber.toFixed(2));
-
-let MyNumber2 = 255;
-
-// toString(2 ~ 36);
-
-console.log(myNumber.toString(2));
-
-console.log(myNumber.toString(8));
-
-console.log(myNumber.toString(16));
+// 길이
+console.log(myString.length);
