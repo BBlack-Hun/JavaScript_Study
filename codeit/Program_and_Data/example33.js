e71bb20d40e3289b7a6b86fd25992506b8e6ef1a
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-21 13:42:03 +0900

    03_21_study

--- codeit/Program_and_Data/example33.js
+++ codeit/Program_and_Data/example33.js
@@ -1,6 +1,34 @@
 function isPalindrome(word) {
-  Test
+  let index = [];
+  let reverse = [];
+  
+  for (i of word) {
+    index.push(i);
+  }
+  reverse = index.slice().reverse();
 
+  let cnt = 0;
+  let flag;
+
+  for (i in word) {
+    if (index[i] != reverse[i]) {
+      flag = 0;
+      break;
+    } else {
+      cnt++;
+      if (cnt == index.length) {
+        flag = 1;
+        break;
+      }
+    }
+  }
+
+  if (flag == 0) {
+    return false;
+  }
+   else {
+     return true;
+   }
 }
 
 // 테스트 코드
