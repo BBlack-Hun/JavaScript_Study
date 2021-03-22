873fa69c8a3e0fc7ca7684fc92112042bf34c795
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-23 00:34:36 +0900

    03_23_study

--- codeit/jquery/topic1/example2.js
+++ codeit/jquery/topic1/example2.js
@@ -1,15 +1,26 @@
 // 주어진 단어(word)에 특정 알파벳(ch)이 몇 번 들어가는지 세어주는 함수
 function countCharacter(word, ch) {
-    var count = 0;
+  var count = 0;
+  var str = word;
+  str = str.toUpperCase();
+  for (let i in str) {
+    if (str[i] == ch) {
+      count++;
+    }
+  }
 
-    // 코드를 작성해주세요.
-
-    return count;
+  return count;
 }
 
 // 단어 word에 알파벳 'A'가 몇 번 나오는지 세어주는 함수
 function countA(word) {
-    // 코드를 작성해주세요.
+  var cnt = 0;
+  for (let i of word) {
+    if (i == 'A' || i == 'a') {
+      cnt++;
+    }
+  }
+  return cnt;
 }
 
 // 테스트 코드
