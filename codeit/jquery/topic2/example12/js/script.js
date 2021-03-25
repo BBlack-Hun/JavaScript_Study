175740880315d51b63964844de786c901d4fbc4d
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-25 21:40:48 +0900

    03_25_study

---
+++ codeit/jquery/topic2/example12/js/script.js
@@ -0,0 +1,19 @@
+$('#even-btn').on('click', function () {
+  $('.card').each(function () {
+    if ($(this).text() % 2 == 0) {
+      $(this).addClass('selected');
+    } else {
+      $(this).removeClass('selected');
+    }
+  });
+});
+
+$('#odd-btn').on('click', function () {
+  $('.card').each(function () {
+    if ($(this).text() % 2 != 0) {
+      $(this).addClass('selected');
+    } else {
+      $(this).removeClass('selected');
+    }
+  });
+});
