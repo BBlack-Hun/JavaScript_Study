738312815dfb78c8dc8c85c152022afdcea27e33
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-26 21:09:46 +0900

    03_26_study

--- codeit/jquery/topic2/example16/js/script.js
+++ codeit/jquery/topic2/example16/js/script.js
@@ -1,22 +1,50 @@
 function scrollHandler() {
-  var scrollBottom = $(window).scrollTop() + $(window).height();
+  var about_scroll = $('.about').offset().top;
 
-  if ($('.header').height() * 2 < scrollBottom) {
+  if ($(window).scrollTop() >= about_scroll) {
     $('.menu-right li button').css('color', '#4a4a4a');
-  } else if ($('.header').height() > $(window).scrollTop()) {
+  } else {
     $('.menu-right li button').css('color', 'white');
   }
 }
 
 $(window).on('scroll', scrollHandler);
-scrollHandler();
 
+scrollHandler();
+var about_scroll = $('.about').offset().top;
+var contact_scroll = $('.contact').offset().top;
 $('#about-btn').on('click', function () {
   var about_scroll = $('.about').offset().top;
   $('html, body').animate({ scrollTop: about_scroll }, 1000);
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= about_scroll) {
+      $('.about .vertical-center').animate({ opacity: 1, top: 0 }, 1000);
+    }
+  });
+});
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() >= about_scroll) {
+    $('.about .vertical-center').animate({ opacity: 1, top: 0 }, 1000);
+    $('.bar .inner-bar').each(function () {
+      var perc = $(this.nextElementSibling).text();
+      $(this).animate({ width: perc }, 500);
+    });
+  }
 });
 
 $('#contact-btn').on('click', function () {
   var contact_scroll = $('.contact').offset().top;
   $('html, body').animate({ scrollTop: contact_scroll }, 1000);
+
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= contact_scroll) {
+      $('.contact .vertical-center').animate({ opacity: 1, top: 0 }, 1000);
+    }
+  });
+});
+
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() >= contact_scroll) {
+    $('.contact .vertical-center').animate({ opacity: 1, top: 0 }, 1000);
+  }
 });
