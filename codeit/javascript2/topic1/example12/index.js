c72fb22da1a513e8f464e6f45f7e4b9061390285
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-29 23:54:56 +0900

    03_29_sutdy

---
+++ codeit/javascript2/topic1/example12/index.js
@@ -0,0 +1,12 @@
+const today = document.querySelector('#today');
+
+today.innerHTML = '<li>처음</li>' + today.innerHTML;
+today.innerHTML = today.innerHTML + '<li>마지막</li>';
+
+today.outerHTML = '<p>이번</p>' + today.outerHTML;
+
+const newToday = document.querySelector('#today');
+newToday.outerHTML = newToday.outerHTML + '<p>다음</p>';
+
+// 요소 노드 추가하기
+const tomorrow = document.querySelector('#tomorrow');
