d2422e371ece614cfcca8bb4bfe142f6f87ff6c4
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-03-30 00:16:18 +0900

    03_30_study

---
+++ codeit/javascript2/topic1/example15/index.js
@@ -0,0 +1,21 @@
+// HTML 속성 (HTML attribute)
+const tomorrow = document.querySelector('#tomorrow');
+const item = tomorrow.firstElementChild;
+const link = item.firstElementChild;
+
+// elem.getAttribute('속성'): 속성에 접근하기
+console.log(tomorrow.getAttributeNode('href'));
+console.log(item.getAttribute('class'));
+
+// id 속성
+console.log(tomorrow);
+console.log(toString.id);
+
+// class 속성
+console.log(item);
+console.log(item.className);
+
+// href 속성
+console.log(link);
+console.log(link.href);
+console.log(tomorrow.href);
