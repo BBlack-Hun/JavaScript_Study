7a119580b2c67df8b2b647eeacdeebd281fb90a4
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-04 13:37:10 +0900

    04_04_study

--- codeit/javascript2/topic2/example1.js
+++ codeit/javascript2/topic2/example1.js
@@ -1,2 +1,26 @@
 // AND와 OR의 연상 방식
 console.log('Codeit' && 'JavaScript');
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log('\n');
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+console.log('\n');
+console.log(null && undefined);
+console.log(0 || true);
+console.log('0' && NaN);
+console.log({} || 123);
+console.log('\n');
+function print(value) {
+  const message = value || 'Codeit';
+
+  console.log(message);
+}
+
+print();
+print('JavaScript');
