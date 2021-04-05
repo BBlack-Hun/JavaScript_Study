f74f703b2225ce3c3b91d3591cd5f339f618cb9f
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-06 00:23:47 +0900

    04_06_study

--- codeit/javascript2/topic2/example13.js
+++ codeit/javascript2/topic2/example13.js
@@ -1,4 +1,18 @@
 // 조건 연산자 (Conditional operator)
+if (조건) {
+  // 조건이 true일 때 동작
+} else {
+  // 조건이 false일 때 동작
+}
+
+switch (값) {
+  case A:
+    // 값이 A와 일치할 때 동작
+    break;
+  default:
+  // 값과 일치하는 case가 없을 때 동작
+}
+
 const CUT_OFF = 80;
 
 function paasChecker(score) {
