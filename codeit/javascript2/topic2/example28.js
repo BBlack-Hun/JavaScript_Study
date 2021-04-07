ddb7db203ec6291fb115cabd99c806f2f4f619b5
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-08 00:01:03 +0900

    04_08_study

---
+++ codeit/javascript2/topic2/example28.js
@@ -0,0 +1,11 @@
+// forEach와 map
+const members = ['영훈', '윤수', '동욱', '태호'];
+for (let member of members) {
+  console.log(`${member}님이 입장하셨습니다.`);
+}
+// const firstNames = ['영훈', '윤수', '동욱', '태호']
+// const lastNames = ['강', '이', '손', '성']
+
+// firstNames.forEach((firstName, i, arr)) => {
+
+// }
