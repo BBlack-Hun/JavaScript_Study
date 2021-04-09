accb9fee3a04f5e4243434dd3330391f7db967bc
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-09 23:55:22 +0900

    04_09_study

---
+++ codeit/javascript2/topic2/example37.js
@@ -0,0 +1,17 @@
+const data = [
+  { company: 'Naber', month: 3 },
+  { company: 'Amajohn', month: 12 },
+  { company: 'Coogle', month: 10 },
+  { company: 'Ittel', month: 6 },
+  { company: 'Sasung', month: 4 },
+  { company: 'CaCao', month: 3 },
+  { company: 'Microhard', month: 17 },
+];
+
+// 여기에 코드를 작성해 주세요.
+const totalCareer = data.reduce((acc, el) => {
+  const { company, month } = el;
+  return acc + month;
+}, 0);
+
+console.log(`상원이의 경력은 총 ${totalCareer}개월입니다.`);
