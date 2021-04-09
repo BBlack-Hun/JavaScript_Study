241ff9c6f4c286c4258af018d6da58a284926dbe
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-09 23:53:53 +0900

    04_09_study

--- codeit/javascript2/topic2/example36.js
+++ codeit/javascript2/topic2/example36.js
@@ -6,4 +6,13 @@ const numbers = [1, 2, 3, 4];
 //   return nextAccvalue;
 // }, initialAccValue);
 
-const sumAll = num;
+const sumAll = numbers.reduce((acc, el, i) => {
+  console.log(`${i}번 index의 요소로 콜백함수가 동작중입니다.`);
+  console.log(`acc:`, acc);
+  console.log('el:', el);
+  console.log('---------------------');
+
+  return acc + el;
+}, 0);
+
+console.log('sumAll: ', sumAll);
