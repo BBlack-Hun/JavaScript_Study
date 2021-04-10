0fac7913ebcad2ed79f59956713cec4f251fcec6
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-10 11:30:23 +0900

    04_10_study

---
+++ codeit/javascript2/topic2/example42/index.js
@@ -0,0 +1,8 @@
+// 모듈 문법
+import { title as printerTitle, print } from './printer.js';
+import { title, data as members } from './members.js';
+
+// const title = 'Codeit';
+
+print(title);
+print(members);
---
+++ codeit/javascript2/topic2/example42/printer.js
@@ -0,0 +1,6 @@
+// 모듈 파일의 조건
+export const title = 'CodeitPrinter';
+
+export function print(value) {
+  console.log(value);
+}
