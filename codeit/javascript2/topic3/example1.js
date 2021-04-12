9eed56a401f659ee8018168456608f5e663cd249
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-12 20:12:06 +0900

    04_12_study

---
+++ codeit/javascript2/topic3/example1.js
@@ -0,0 +1,19 @@
+console.log('Hello, World!');
+
+const user = {
+  email: 'chris123@google.com',
+  birthdate: '1992-03-21',
+
+  buy(item) {
+    console.log(`${this.email}. buys ${this.name}`);
+  },
+};
+
+const item = {
+  name: '스웨터',
+  price: 30000,
+};
+
+console.log(user.email);
+console.log(user.birthdate);
+user.buy(item);
---
+++ codeit/javascript2/topic3/example2.js
@@ -0,0 +1,17 @@
+// 객체 만들기 1-2: Factory Function
+
+// const user1 = {
+//   email: 'chris123@google.com',
+//   birthdate: '1992-03-21',
+//   buy(item) {
+//     console.log(`${this.email}. buys ${this.name}`);
+//   },
+// };
+
+// const user2 = {
+//   email: 'jerry99@google.com',
+//   birthdate: '1997-07-19',
+//   buy(item) {
+//     console.log(`${this.email}. buys ${this.name}`);
+//   },
+// };
