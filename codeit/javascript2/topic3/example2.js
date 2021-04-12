d0ba894ce4c50650fc4b5d50d726aaa22aab0d5e
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-12 20:18:48 +0900

    04_12_study

--- codeit/javascript2/topic3/example2.js
+++ codeit/javascript2/topic3/example2.js
@@ -4,7 +4,7 @@
 //   email: 'chris123@google.com',
 //   birthdate: '1992-03-21',
 //   buy(item) {
-//     console.log(`${this.email}. buys ${this.name}`);
+//     console.log(`${this.email}. buys ${item.name}`);
 //   },
 // };
 
@@ -12,6 +12,33 @@
 //   email: 'jerry99@google.com',
 //   birthdate: '1997-07-19',
 //   buy(item) {
-//     console.log(`${this.email}. buys ${this.name}`);
+//     console.log(`${this.email}. buys ${item.name}`);
 //   },
 // };
+
+function createUser(email, birthdate) {
+  const user = {
+    email: email,
+    birthdate: birthdate,
+    buy(item) {
+      console.log(`${this.email} buys ${item.name}`);
+    },
+  };
+  return user;
+}
+const item = {
+  name: '스웨터',
+  price: 30000,
+};
+
+const user1 = createUser('chris123@google.com', '1992-03-21');
+const user2 = createUser('jerry99@google.com', '1995-07-19');
+const user3 = createUser('alice@google.com', '1993-12-24');
+
+console.log(user1.email);
+console.log(user2.email);
+console.log(user3.email);
+
+user1.buy(item);
+user2.buy(item);
+user3.buy(item);
