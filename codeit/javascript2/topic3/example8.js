c157fbc3367907f88afbbe976eac39d07c72a02b
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-13 20:46:51 +0900

    04_13_study

--- codeit/javascript2/topic3/example8.js
+++ codeit/javascript2/topic3/example8.js
@@ -10,6 +10,10 @@ class User {
     console.log(`${this.email} buys ${item.name}`);
   }
 
+  get email() {
+    return this._email;
+  }
+
   set email(address) {
     if (address.includes('@')) {
       this._email = address;
@@ -24,7 +28,7 @@ const item = {
   price: 30000,
 };
 const user1 = new User('chris123@google.com', '1992-03-21');
-user1.email = 'chris robert';
+user1.email = 'chris_robert@google.com';
 
 console.log(user1.email);
 console.log(user1.birthdate);
