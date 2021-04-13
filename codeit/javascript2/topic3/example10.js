4f4372a7ddc5f7e977c5d1f4e436904f1e192901
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-13 20:51:49 +0900

    04_13_study

---
+++ codeit/javascript2/topic3/example10.js
@@ -0,0 +1,35 @@
+// 캡슐화
+
+class User {
+  constructor(email, birthdate) {
+    this.email = email;
+    this.birthdate = birthdate;
+  }
+
+  buy(item) {
+    console.log(`${this.email} buys ${item.name}`);
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  set email(address) {
+    if (address.includes('@')) {
+      this._email = address;
+    } else {
+      throw new Error('invalid email address');
+    }
+  }
+}
+
+const item = {
+  name: '스웨터',
+  price: 30000,
+};
+const user1 = new User('chris123@google.com', '1992-03-21');
+user1.email = 'chris_robert@google.com';
+
+console.log(user1.email);
+console.log(user1.birthdate);
+user1.buy(item);
