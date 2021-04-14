f6485dfd8cef78929657a2a2b34152ef4b645d96
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-14 20:49:17 +0900

    04_14_study

--- codeit/javascript2/topic3/example7/main.js
+++ codeit/javascript2/topic3/example7/main.js
@@ -1,13 +1,16 @@
+// 은행 클래스
 class BankAccount {
+  // 생성자 - 계좌주, 계좌(현재 잔액)
   constructor(name, money) {
     this.holder = name;
     this.balance = money;
   }
-
+  // 입금
   deposit(money) {
     this.balance += money;
   }
 
+  // 출금
   withdraw(money) {
     if (this.balance - money < 0) {
       console.log('Insufficient balance');
@@ -15,7 +18,7 @@ class BankAccount {
       this.balance -= money;
     }
   }
-
+  // 송금
   transfer(money, anotherAccount) {
     const account = anotherAccount;
     if (this.balance - money < 0) {
