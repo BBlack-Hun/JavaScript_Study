a3463eac542444242e43d4578af17ed208c118a2
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-20 00:06:17 +0900

    04_20_study

--- codeit/javascript2/topic4/example20.js
+++ codeit/javascript2/topic4/example20.js
@@ -1,3 +1,13 @@
 // 직접 만들어보는 Promise 객체
 
-const p = new Promise((resolve, reject) => {});
+const p = new Promise((resolve, reject) => {
+  // setTimeout(() => {
+  //   resolve('success');
+  // }, 2000);
+  setTimeout(() => {
+    reject(new Error('fail'));
+  }, 2000);
+});
+p.then((result) => {
+  console.log(result);
+});
