c6fe272775fb984fd28c41f24fbd4457f036e04b
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-20 23:33:01 +0900

    04_20_study

---
+++ codeit/javascript2/topic4/example27.js
@@ -0,0 +1,36 @@
+// async 함수 안의 async함수
+
+const applyPrivacyRule = async function (users) {
+  const resultWithRuleApplied = users.map((user) => {
+    const keys = Object.keys(user);
+    const userWithoutPrivateInfo = {};
+    keys.forEach((key) => {
+      if (key !== 'address' && key !== 'phone') {
+        userWithoutPrivateInfo[key] = user[key];
+      }
+    });
+    return userWithoutPrivateInfo;
+  });
+  const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(resultWithRuleApplied);
+    }, 2000);
+  });
+  return p;
+};
+
+async function getUsers() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const result = await response.text();
+    const users = JSON.parse(result);
+    const resultWithPrivacyReulApplied = await applyPrivacyRule(users);
+    return resultWithPrivacyReulApplied;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('exit');
+  }
+}
+
+getUsers().then((result) => console.log(result));
