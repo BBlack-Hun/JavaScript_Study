6fdd378e64946cd1836a64de19fbe92bdc248f91
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-21 21:55:01 +0900

    04_21_study

---
+++ codeit/nodejs/topic1/example6/main.js
@@ -0,0 +1,13 @@
+// 코어 모듈
+
+// let m = require('./math-tools.js');
+// const fs = require('fs');
+
+// // let fileList = fs.readdirSync('.');
+// // console.log(fileList);
+
+// fs.writeFileSync('new', 'Hello Node.js!');
+
+const os = require('os');
+
+console.log(os.cpus());
