87ecea7fd1e2618a586f7945287ff405a5ef9363
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-22 22:46:10 +0900

    04_22_study

---
+++ codeit/nodejs/topic1/example8/main.js
@@ -0,0 +1,20 @@
+// 이상한 출력 순서
+
+const fs = require('fs');
+
+// 동기 실행
+// console.log('Start!');
+
+// let content = fs.readFileSync('./new', 'utf8');
+// console.log(content);
+
+// console.log('Finish!');
+
+// 비동기 실행
+console.log('Start!');
+
+fs.readFile('./new', 'utf8', (error, data) => {
+  console.log(data);
+});
+
+console.log('Finish!');
